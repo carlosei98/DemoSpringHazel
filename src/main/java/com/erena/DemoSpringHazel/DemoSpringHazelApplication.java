ae83bec70039b836fda76683ef25a9fe6d9b5d58
Carlos Erena Ibáñez <carlos.erena.ibanez@iesjandula.es>
2020-09-26 11:10:57 +0200

    Comienzo del proyecto.

---
+++ src/main/java/com/erena/DemoSpringHazel/DemoSpringHazelApplication.java
@@ -0,0 +1,13 @@
+package com.erena.DemoSpringHazel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringHazelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringHazelApplication.class, args);
+	}
+
+}
